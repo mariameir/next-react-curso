96d61779aef231731f916d805a8e2c92b61b799d
Maria Meir <pigyarx@gmail.com>
2023-09-19 16:05:06 -0400

    global.css font family

---
+++ pages/basicos/jsx4.jsx
@@ -0,0 +1,9 @@
+export default function biluteteia (){
+  return(
+    <div>
+      <h1>Lorem ipsum dolor sit.</h1>
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et accusamus dolor ea?</p>
+    </div>
+  )
+}
