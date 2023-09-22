48dad49872f9351af63b0cadfcee2f7176b0ffdc
Maria Meir <pigyarx@gmail.com>
2023-09-22 13:11:17 -0400

    teste

--- pages/basicos/jsx4.jsx
+++ pages/basicos/jsx4.jsx
@@ -4,6 +4,8 @@ export default function biluteteia (){
       <h1>Lorem ipsum dolor sit.</h1>
       <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et accusamus dolor ea?</p>
+      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum vitae ex aliquam eaque asperiores voluptatum, ratione quas sequi praesentium animi hic debitis recusandae, maxime tenetur totam blanditiis provident magnam molestiae.
+      </p>
     </div>
   )
 }
