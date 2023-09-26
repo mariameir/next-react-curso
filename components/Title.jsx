69ad776175891c64647ec83d2f3c0f8a4c8b09f3
Maria Meir <pigyarx@gmail.com>
2023-09-26 16:44:27 -0400

    fragment

---
+++ components/Title.jsx
@@ -0,0 +1,5 @@
+export default function Title(){
+  return(
+  <h1>Sociedade Espotiva Palmeiras</h1>
+  )
+}
---
+++ pages/basicos/fragment.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+export default function fragment () {
+  return(
+    <React.Fragment>
+    <h1>Lorem</h1>
+    <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, fugit mollitia minus praesentium esse reprehenderit distinctio ipsa repellat rem quo ipsum sit. Non consequatur eos, iste accusantium esse sint repellendus.</h2>
+    </React.Fragment>
+  )
+  
+}
